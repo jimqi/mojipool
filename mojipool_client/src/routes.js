23224dd977c8fe1985cef6d7c780318a8ebbbb50
Jim Qi <jim.qi101@gmail.com>
2017-03-18 17:34:25 -0700

    Fixed structure and more scaffolding

---
+++ mojipool_client/src/routes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+
+import App from './components/App';
+
+const Placeholder = () => {
+    return <div> Under Construction </div>
+}
+
+export default (
+    <Route path="/" component={App} >
+        <IndexRoute component={Placeholder} />
+    </Route>
+)
